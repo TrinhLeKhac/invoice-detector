8300e671a2939a219870c25ddc2c701d3d919431
TrinhLeKhac <emperorfire52@gmail.com>
2025-03-16 16:59:25 +0700

    Fix regex

--- api/utils/text/regex.py
+++ api/utils/text/regex.py
@@ -1,5 +1,5 @@
 SPECIAL_CHARACTER = r"=><„,.:;“”\/-_(){}|?"
-VOCAB = r'aAàÀảẢãÃáÁạẠăĂằẰẳẲẵẴắẮặẶâÂầẦẩẨẫẪấẤậẬbBcCdDđĐeEèÈẻẺẽẼéÉẹẸêÊềỀểỂễỄếẾệỆfFgGhHiIìÌỉỈĩĨíÍịỊjJkKlLmMnNoOòÒỏỎõÕóÓọỌôÔồỒổỔỗỖốỐộỘơƠờỜởỞỡỠớỚợỢpPqQrRsStTuUùÙủỦũŨúÚụỤưƯừỪửỬữỮứỨựỰvVwWxXyYỳỲỷỶỹỸýÝỵỴzZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
+VOCAB = r'aAáÁàÀảẢãÃạẠăĂắẮằẰẳẲẵẴặẶâÂấẤầẦẩẨẫẪậẬbBcCdDđĐeEéÉèÈẻẺẽẼẹẸêÊếẾềỀểỂễỄệỆfFgGhHiIíÍìÌỉỈĩĨịỊjJkKlLmMnNoOóÓòÒỏỎõÕọỌôÔốỐồỒổỔỗỖộỘơƠớỚờỜởỞỡỠợỢpPqQrRsStTuUúÚùÙủỦũŨụỤưƯứỨừỪửỬữỮựỰvVwWxXyYýÝỳỲỷỶỹỸỵỴzZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
 
 DIGIT_AND_MISSPELLED_CHAR = "\dOÔƠ,\.\s"
 OPTIONAL_COLON = "\s*:?\s*"
@@ -8,35 +8,35 @@ OPTIONAL_COLON = "\s*:?\s*"
 TIME_DATE_PATTERN = r"(?:(\d{2}[:]\d{2}(?::\d{2})?)\s+)?(\d{2}[\s/-]\d{2}[\s/-]\d{4})(?:\s+(\d{2}[:]\d{2}(?::\d{2})?))?"
 
 # CREATED_TIME_PATTERN = r"(?:(?:hoa don ban hang)[^0-9]*)?(\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4}(?:\s+\d{1,2}:\d{2}(?::\d{2})?)?|\d{1,2}:\d{2}(?::\d{2})?\s+\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4})"
-CREATED_TIME_PATTERN = r"(?:(?:h[oòóỏọõôốồổỗộơờớởỡợ]\s*[aàáãạâấầẩẫậăắằẳẵặ]\s*[dđ]\s*[oòóỏọõôốồổỗộơờớởỡợ]\s*[mn]\s*b[aàáãạâấầẩẫậăắằẳẵặ]\s*[mn]\s*h[aàáãạâấầẩẫậăắằẳẵặ]\s*[mn][qg])[^0-9]*)?(\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4}(?:\s+\d{1,2}:\d{2}(?::\d{2})?)?|\d{1,2}:\d{2}(?::\d{2})?\s+\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4})"
+CREATED_TIME_PATTERN = r"(?:(?:h[oóòỏõọôốồổỗộơớờởỡợ]\s*[aáàảãạăắằẳẵặâấầẩẫậ]\s*[dđ]\s*[oóòỏõọôốồổỗộơớờởỡợ]\s*[mn]\s*b[aáàảãạăắằẳẵặâấầẩẫậ]\s*[mn]\s*h[aáàảãạăắằẳẵặâấầẩẫậ]\s*[mn][qg])[^0-9]*)?(\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4}(?:\s+\d{1,2}:\d{2}(?::\d{2})?)?|\d{1,2}:\d{2}(?::\d{2})?\s+\d{1,2}\s*[/:.-]\s*\d{1,2}\s*[/:.-]\s*\d{4})"
 
 # SHOP_NAME_PATTERN = rf"(?:s\s*hop){OPTIONAL_COLON}(.*?)(?:hot\s*line)"
-SHOP_NAME_PATTERN = rf"(?:s\s*h[oòóỏọõôốồổỗộơờớởỡợ]p){OPTIONAL_COLON}(.*?)(?:h[oòóỏọõôốồổỗộơờớởỡợ]t\s*l[iìíĩịîï][mn][êeéèẽẹêềếểễệ])"
+SHOP_NAME_PATTERN = rf"(?:s\s*h[oóòỏõọôốồổỗộơớờởỡợ]p){OPTIONAL_COLON}(.*?)(?:h[oóòỏõọôốồổỗộơớờởỡợ]t\s*l[iíìỉĩịîï][mn][eéèẻẽẹêếềểễệ])"
 
 # HOTLINE_PATTERN = rf"(?:hot\s*line){OPTIONAL_COLON}(.*?)(?:nhan\s*vien\s*ban\s*hang)"
-HOTLINE_PATTERN = rf"(?:h[oòóỏọõôốồổỗộơờớởỡợ]t\s*l[iìíĩịîï][mn][êeéèẽẹêềếểễệ]){OPTIONAL_COLON}(.*?)(?:[mn]h[aàáãạâấầẩẫậăắằẳẵặ][mn]\s*v[iìíĩịîï][êeéèẽẹêềếểễệ]n\s*b[aàáãạâấầẩẫậăắằẳẵặ][mn]\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg])"
+HOTLINE_PATTERN = rf"(?:h[oóòỏõọôốồổỗộơớờởỡợ]t\s*l[iíìỉĩịîï][mn][eéèẻẽẹêếềểễệ]){OPTIONAL_COLON}(.*?)(?:[mn]h[aáàảãạăắằẳẵặâấầẩẫậ][mn]\s*v[iíìỉĩịîï][eéèẻẽẹêếềểễệ]n\s*b[aáàảãạăắằẳẵặâấầẩẫậ][mn]\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg])"
 
 # EMPLOYEE_NAME_PATTERN = rf"(?:nhan\s*vien\s*ban\s*hang){OPTIONAL_COLON}(.*?)(?:khach\s*hang)"
-EMPLOYEE_NAME_PATTERN = rf"(?:[mn]h[aàáãạâấầẩẫậăắằẳẵặ][mn]\s*v[iìíĩịîï][êeéèẽẹêềếểễệ]n\s*b[aàáãạâấầẩẫậăắằẳẵặ][mn]\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg]){OPTIONAL_COLON}(.*?)(?:kh[aàáãạâấầẩẫậăắằẳẵặ]ch\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg])"
+EMPLOYEE_NAME_PATTERN = rf"(?:[mn]h[aáàảãạăắằẳẵặâấầẩẫậ][mn]\s*v[iíìỉĩịîï][eéèẻẽẹêếềểễệ]n\s*b[aáàảãạăắằẳẵặâấầẩẫậ][mn]\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg]){OPTIONAL_COLON}(.*?)(?:kh[aáàảãạăắằẳẵặâấầẩẫậ]ch\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg])"
 
 # CUSTOMER_NAME_PATTERN = rf"(?:khach\s*hang){OPTIONAL_COLON}(.*?)(?:s\s*d[t|f])"
-CUSTOMER_NAME_PATTERN = rf"(?:kh[aàáãạâấầẩẫậăắằẳẵặ]ch\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg]){OPTIONAL_COLON}(.*?)(?:s\s*[dđ][t|f])"
+CUSTOMER_NAME_PATTERN = rf"(?:kh[aáàảãạăắằẳẵặâấầẩẫậ]ch\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg]){OPTIONAL_COLON}(.*?)(?:s*\s*[dđ][t|f])"
 
 # CUSTOMER_PHONE_PATTERN = rf"(?:s\s*d[t|f]){OPTIONAL_COLON}(.*?)(?:dia\s*chi)"
-CUSTOMER_PHONE_PATTERN = rf"(?:s\s*[dđ][t|f]){OPTIONAL_COLON}(.*?)(?:[dđ][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ]\s*ch[iìíĩịîï])"
+CUSTOMER_PHONE_PATTERN = rf"(?:s*\s*[dđ][t|f]){OPTIONAL_COLON}(.*?)(?:[dđ][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ]\s*ch[iíìỉĩịîï]*)"
 
 # ADDRESS_PATTERN = rf"(?:dia\s*chi){OPTIONAL_COLON}(.*?)(?:khu\s*vuc)"
-ADDRESS_PATTERN = rf"(?:[dđ][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ]\s*ch[iìíĩịîï]){OPTIONAL_COLON}(.*?)(?:kh[uùúủũụưừứửữự]\s*v[uùúủũụưừứửữự]c)"
+ADDRESS_PATTERN = rf"(?:[dđ][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ]\s*ch[iíìỉĩịîï]*){OPTIONAL_COLON}(.*?)(?:kh[uúùủũụưứừửữự]\s*v[uúùủũụưứừửữự]c)"
 
 # REGION_PATTERN = rf"(?:khu\s*vuc){OPTIONAL_COLON}(.*?)(?:thoi\s*gian\s*giao\s*hang)"
-REGION_PATTERN = rf"(?:kh[uùúủũụưừứửữự]\s*v[uùúủũụưừứửữự]c){OPTIONAL_COLON}(.*?)(?:th[oòóỏọõôốồổỗộơờớởỡợ][iìíĩịîï]\s*[qg][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ]n\s*[qg][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ][oòóỏọõôốồổỗộơờớởỡợ]\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg])"
+REGION_PATTERN = rf"(?:kh[uúùủũụưứừửữự]\s*v[uúùủũụưứừửữự]c){OPTIONAL_COLON}(.*?)(?:th[oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩịîï]\s*[qg][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ]n\s*[qg][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ][oóòỏõọôốồổỗộơớờởỡợ]\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg])"
 
 # SHIPPING_TIME_PATTERN = rf"(?:thoi\s*gian\s*giao\s*hang){OPTIONAL_COLON}(.*?)(?:ten)"
-SHIPPING_TIME_PATTERN = rf"(?:th[oòóỏọõôốồổỗộơờớởỡợ][iìíĩịîï]\s*[qg][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ]n\s*[qg][iìíĩịîï][aàáãạâấầẩẫậăắằẳẵặ][oòóỏọõôốồổỗộơờớởỡợ]\s*h[aàáãạâấầẩẫậăắằẳẵặ][mn][qg]){OPTIONAL_COLON}(.*?)(?:t[êeéèẽẹêềếểễệ][mn])"
+SHIPPING_TIME_PATTERN = rf"(?:th[oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩịîï]\s*[qg][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ]n\s*[qg][iíìỉĩịîï][aáàảãạăắằẳẵặâấầẩẫậ][oóòỏõọôốồổỗộơớờởỡợ]\s*h[aáàảãạăắằẳẵặâấầẩẫậ][mn][qg]){OPTIONAL_COLON}(.*?)(?:t[eéèẻẽẹêếềểễệ][mn])"
 
 TOTAL_QUANTITY_PATTERN = rf"t[ôoòóỏọõốồổỗộơờớởỡợ]ng\s*s[ôoòóỏọõốồổỗộơờớởỡợ]\s*l[uưừứửữự][oôọóòõốồổỗộơờớởỡợ]ng{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
-TOTAL_AMOUNT_PATTERN = rf"t[ôoòóỏọõốồổỗộơờớởỡợ]ng\s*t[iìíĩịîï][êeéèẽẹêềếểễệ]n\s*h[aàáãạâấầẩẫậăắằẳẵặ]ng{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
-DISCOUNT_PATTERN = rf"ch[iìíĩịîï][êeéèẽẹêềếểễệ]t\s*kh[aàáãạâấầẩẫậăắằẳẵặ][uưừứửữự]\s*h[oòóỏọõôốồổỗộơờớởỡợ][aàáãạâấầẩẫậăắằẳẵặ]\s*[đd][oòóỏọõôốồổỗộơờớởỡợ]n{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
+TOTAL_AMOUNT_PATTERN = rf"t[ôoòóỏọõốồổỗộơờớởỡợ]ng\s*t[iíìỉĩịîï][eéèẻẽẹêếềểễệ]n\s*h[aáàảãạăắằẳẵặâấầẩẫậ]ng{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
+DISCOUNT_PATTERN = rf"ch[iíìỉĩịîï][eéèẻẽẹêếềểễệ]t\s*kh[aáàảãạăắằẳẵặâấầẩẫậ][uưừứửữự]\s*h[oóòỏõọôốồổỗộơớờởỡợ][aáàảãạăắằẳẵặâấầẩẫậ]\s*[đd][oóòỏõọôốồổỗộơớờởỡợ]n{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
 MONETARY_PATTERN = rf"t[ôoòóỏọõốồổỗộơờớởỡợ]ng\s*c[oôòóỏọõốồổỗộơờớởỡợ]ng{OPTIONAL_COLON}([{DIGIT_AND_MISSPELLED_CHAR}]+)"
 
 TABLE_COLUMN_MAPPING = {
